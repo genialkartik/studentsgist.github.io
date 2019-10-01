a8824446ca1a8721ab691c6f2a9f8fd25bffacd8
Kartik Tyagi <32240906+genialkartik@users.noreply.github.com>
2019-10-02 05:10:13 +0530

    Update template.js

--- javascript/template.js
+++ javascript/template.js
@@ -1,9 +1,9 @@
 var temp = 0;
-function currentimg( leftvalue ){
+function currentimg( leftvalue ){   //leftvalue is the current position of image
   temp+=leftvalue;
 var j= 0;
-var imageposition = document.getElementsByClassName("product-image");
-  if( temp < -75){
+var imageposition = document.getElementsByClassName("product-image"); //changing postion of image
+  if( temp < -75){      //when image is at 75% of width
     temp = 0;
     for(j=0; j < imageposition.length; j++){
       imageposition[j].style.left = temp + "%";
