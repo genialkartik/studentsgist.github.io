450a0d141dd630bc247de5c306c676debf03701d
Kartik Tyagi <kartik9756@gmail.com>
2019-10-29 14:14:08 +0530

    Create product-detail-script.js

---
+++ xampp/javascript/product-detail-script.js
@@ -0,0 +1,23 @@
+var temp = 0;
+function procurrentimg( leftvalue ){   //leftvalue is the current position of image
+  temp+=leftvalue;
+var j= 0;
+var imageposition = document.getElementsByClassName("pro-duct-img"); //changing postion of image
+  if( temp < -75){      //when image is at 75% of width
+    temp = 0;
+    for(j=0; j < imageposition.length; j++){
+      imageposition[j].style.left = temp + "%";
+    }
+  }
+  else if( temp > 0){
+    temp = -75;
+    for(j=0; j < imageposition.length; j++){
+      imageposition[j].style.left = temp + "%";
+    }
+  }
+  else{
+    for(j=0; j < imageposition.length; j++){
+      imageposition[j].style.left = temp + "%";
+    }
+  }
+}
